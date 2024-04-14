30c34e89be65edf1d043c6382e488e469c69a86b
XianReallyHot-ZZH <741788769@qq.com>
2024-04-14 10:59:56 +0800

    init:项目结构初始化

---
+++ src/main/java/cn/youyou/yyregistry/YyregistryApplication.java
@@ -0,0 +1,13 @@
+package cn.youyou.yyregistry;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class YyregistryApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(YyregistryApplication.class, args);
+    }
+
+}
---
+++ src/test/java/cn/youyou/yyregistry/YyregistryApplicationTests.java
@@ -0,0 +1,13 @@
+package cn.youyou.yyregistry;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class YyregistryApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
